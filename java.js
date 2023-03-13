8df954538ffa99118396ac86c08c48b549ff0f50
kalmansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-03-13 13:20:09 +0800

    Added rounds counter

--- java.js
+++ java.js
@@ -1,10 +1,5 @@
-let promptinput = prompt('')
-
-const array = ['rock', 'paper', 'scissor'];
-
-const compinput = getRandomItem(array);
-
-getComputerChoice(promptinput, compinput);
+let round = 1
+start();
 
 function getRandomItem(arr) {
 
@@ -17,20 +12,40 @@ function getRandomItem(arr) {
     return item;
 }
 
-
 function getComputerChoice(playerSelection, getComputerChoice) {
     let playerinput = playerSelection.toLowerCase;
-    
 
-    if (playerSelection === getComputerChoice) {
+    if (playerSelection === getComputerChoice && round != 6) {
         alert("Computer picked " + getComputerChoice + ". It's a draw!");
+        round = round + 1
+        start();
     } else if (
-        (playerSelection === 'rock' && getComputerChoice === 'scissor') || (playerSelection === 'paper' && getComputerChoice === 'rock') || (playerSelection === 'scissor' && getComputerChoice === 'paper')
+        (playerSelection === 'rock' && getComputerChoice === 'scissor') || (playerSelection === 'paper' && getComputerChoice === 'rock') || (playerSelection === 'scissor' && getComputerChoice === 'paper') && round != 6
         ) {
         alert("Computer picked " + getComputerChoice + ". You win!");
+        round = round + 1
+        start();
     } else if (
-        (playerSelection === 'rock' && getComputerChoice === 'paper') || (playerSelection === 'paper' && getComputerChoice === 'scissor') || (playerSelection === 'scissor' && getComputerChoice === 'rock')
+        (playerSelection === 'rock' && getComputerChoice === 'paper') || (playerSelection === 'paper' && getComputerChoice === 'scissor') || (playerSelection === 'scissor' && getComputerChoice === 'rock') && round != 6
         ) {
         alert("Computer picked " + getComputerChoice + ". You lose!");
+        round = round + 1
+        start();
     }
+    return round
+}
+
+function game() {
+    if (round == 6) {
+    alert('Game Over')
+    } else {
+    return prompt('Round ' + round + '. Choose your destiny.', '')
+    }  
+}
+
+function start() {
+    const array = ['rock', 'paper', 'scissor'];
+    const compinput = getRandomItem(array);
+    promptinput = game();
+    getComputerChoice(promptinput, compinput);
 }
