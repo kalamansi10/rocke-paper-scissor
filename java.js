f914984f441f4bc1f73d54458b044ef372b8b1e7
kalmansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-05-01 13:34:52 +0800

    First attempt to adding UI WIP

--- java.js
+++ java.js
@@ -1,5 +1,39 @@
 let round = 1
-start();
+let playerscore = 0
+let computerscore = 0
+
+const rockbtn = document.querySelector('.rock');
+const paperbtn = document.querySelector('.paper');
+const scissorbtn = document.querySelector('.scissor');
+
+rockbtn.addEventListener('click', function (e) {
+    if (round == 6) {
+        alert('Game Over')
+        } else {
+            const array = ['rock', 'paper', 'scissor'];
+            const compinput = getRandomItem(array);
+            getComputerChoice('rock', compinput);
+        }
+            
+});
+paperbtn.addEventListener('click', function (e) {
+    if (round == 6) {
+        alert('Game Over')
+        } else {
+            const array = ['rock', 'paper', 'scissor'];
+            const compinput = getRandomItem(array);
+            getComputerChoice('paper', compinput);
+        }
+}); 
+scissorbtn.addEventListener('click', function (e) {
+    if (round == 6) {
+        alert('Game Over')
+        } else {
+            const array = ['rock', 'paper', 'scissor'];
+            const compinput = getRandomItem(array);
+            getComputerChoice('scissor', compinput);
+        }
+}); 
 
 function getRandomItem(arr) {
 
@@ -12,40 +46,39 @@ function getRandomItem(arr) {
     return item;
 }
 
+
 function getComputerChoice(playerSelection, getComputerChoice) {
     let playerinput = playerSelection.toLowerCase;
 
     if (playerSelection === getComputerChoice && round != 6) {
         alert("Computer picked " + getComputerChoice + ". It's a draw!");
         round = round + 1
-        start();
+        document.getElementById("round").textContent = `Round ${round}`;
+        document.getElementById("score").textContent = `${playerscore} - ${computerscore}`;
     } else if (
         (playerSelection === 'rock' && getComputerChoice === 'scissor') || (playerSelection === 'paper' && getComputerChoice === 'rock') || (playerSelection === 'scissor' && getComputerChoice === 'paper') && round != 6
         ) {
         alert("Computer picked " + getComputerChoice + ". You win!");
         round = round + 1
-        start();
+        playerscore = playerscore + 1
+        document.getElementById("round").textContent = `Round ${round}`;
+        document.getElementById("score").textContent = `${playerscore} - ${computerscore}`;
     } else if (
         (playerSelection === 'rock' && getComputerChoice === 'paper') || (playerSelection === 'paper' && getComputerChoice === 'scissor') || (playerSelection === 'scissor' && getComputerChoice === 'rock') && round != 6
         ) {
         alert("Computer picked " + getComputerChoice + ". You lose!");
         round = round + 1
-        start();
+        computerscore = computerscore + 1
+        document.getElementById("round").textContent = `Round ${round}`;
+        document.getElementById("score").textContent = `${playerscore} - ${computerscore}`;
     }
     return round
 }
 
-function game() {
-    if (round == 6) {
-    alert('Game Over')
-    } else {
-    return prompt('Round ' + round + '. Choose your destiny.', '')
-    }  
-}
 
-function start() {
-    const array = ['rock', 'paper', 'scissor'];
-    const compinput = getRandomItem(array);
-    promptinput = game();
-    getComputerChoice(promptinput, compinput);
-}
+
+  
+
+        
+
+    
